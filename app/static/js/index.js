839fe7cc22270481f41a9390b3394b34d9c9395a
sanix-darker <s4nixd@gmail.com>
2021-01-16 20:02:08 +0100

    feat: Command copied

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -56,6 +56,29 @@ function makeAceEditorResizable(editor){
     });
 }
 
+function copyToClipboard(text, el) {
+    var copyTest = document.queryCommandSupported('copy');
+    var elOriginalText = el.attr('data-original-title');
+
+    if (copyTest === true) {
+    var copyTextArea = document.createElement("textarea");
+    copyTextArea.value = text;
+    document.body.appendChild(copyTextArea);
+    copyTextArea.select();
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'Copied!' : 'Whoops, not copied!';
+        el.attr('data-original-title', msg).tooltip('show');
+    } catch (err) {
+        console.log('Oops, unable to copy');
+    }
+    document.body.removeChild(copyTextArea);
+    el.attr('data-original-title', elOriginalText);
+    } else {
+    // Fallback if browser doesn't support .execCommand('copy')
+    window.prompt("Copy to clipboard: Ctrl+C or Command+C, Enter", text);
+    }
+}
 
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
@@ -70,9 +93,14 @@ $(document).ready(function(){
 
     makeAceEditorResizable(editor);
 
-
     // collapse accordion 1
     $("#edit").trigger("click");
-
+    // To generate the data-table
     $('#mylist').DataTable();
+
+    $('#co').click(function() {
+        var text = document.querySelector('#co-input').innerHTML;
+        var el = $(this);
+        copyToClipboard(text, el);
+    });
 });
